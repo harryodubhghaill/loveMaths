9ecd4928bf927d8fc40bafd6558cb3786c682cb1
harryodubhghaill <harryodubhghaill@hotmail.com>
2021-12-20 01:37:07 +0000

    Add code for sunstract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ function runGame(gameType){
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'multiply'){
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unkown game type: ${gameType}`);
         throw `Unkown game type: ${gameType}. Aborting!`;
@@ -62,6 +64,8 @@ function calculateCorrectAnswer(){
         return [operand1 + operand2, 'addition'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
     } else {
         alert(`Unimplimented operator ${operator}`);
         throw `Unimplimented operator ${operator}. Aborting!`;
@@ -89,8 +93,8 @@ function displayAdditionQuestion(operand1, operand2){
 }
 
 function displaySubtractQuestion(operand1, operand2){
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
